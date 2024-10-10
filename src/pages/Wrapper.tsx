85ee894199caef51a06670b34f8d5d351ad938bd
Arcylisz <olek1305@gmail.com>
2024-10-10 08:23:04 +0200

    fix await user if user is logged

--- src/pages/Wrapper.tsx
+++ src/pages/Wrapper.tsx
@@ -4,22 +4,25 @@ import axios from 'axios';
 import { User } from "../classes/user";
 import { connect } from "react-redux";
 import { setUser } from "../redux/reducers/userSlice"
+import {RootState} from "../redux/configureStore";
 
 interface Props {
+    user: User | null;
     setUser: (user: User) => void;
     children: React.ReactNode;
 }
 
 class Wrapper extends Component<Props> {
     componentDidMount = async () => {
-        try {
-            const response = await axios.get('user');
-
-            const userData = response.data.data;
-
-            this.props.setUser(userData);
-        } catch (e) {
-            console.error("Error fetching user:", e);
+        // Only fetch user if user is not already set in Redux
+        if (!this.props.user) {
+            try {
+                const response = await axios.get('user');
+                const userData = response.data.data;
+                this.props.setUser(userData); // Set user in Redux store
+            } catch (e) {
+                console.error("Error fetching user:", e);
+            }
         }
     }
 
@@ -35,12 +38,17 @@ class Wrapper extends Component<Props> {
     }
 }
 
-
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => {
+const mapStateToProps = (state: RootState) => {
     return {
-        setUser: (user: User) => dispatch(setUser(user))
+        user: state.user, // Get user from Redux state
     };
 };
 
-export default connect(null, mapDispatchToProps)(Wrapper);
+const mapDispatchToProps = (dispatch: Dispatch<any>) => {
+    return {
+        setUser: (user: User) => dispatch(setUser(user)),
+    };
+};
+
+// Connect component to Redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Wrapper);
