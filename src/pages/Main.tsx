7c900d2cc5c59b717330ac120a2496d63eb23bf6
Arcylisz <olek1305@gmail.com>
2024-10-10 13:49:01 +0200

    add main component, search and products

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,83 +1,124 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import Wrapper from "./Wrapper";
 import Header from "../components/Header";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import { RootState } from '../redux/configureStore';
+import {fetchProducts} from "../redux/reducers/productSlice";
 
-class Main extends Component {
-    render() {
-        return (
-            <Wrapper>
-                <Header/>
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div className="col">
-                        <div className="card shadow-sm">
-                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
-                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                                <rect width="100%" height="100%" fill="#55595c"/>
-                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                            </svg>
-                            <div className="card-body">
-                                <p className="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="btn-group">
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small className="text-body-secondary">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card shadow-sm">
-                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
-                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                                <rect width="100%" height="100%" fill="#55595c"/>
-                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                            </svg>
-                            <div className="card-body">
-                                <p className="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="btn-group">
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small className="text-body-secondary">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card shadow-sm">
-                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
-                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                                <rect width="100%" height="100%" fill="#55595c"/>
-                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                            </svg>
-                            <div className="card-body">
-                                <p className="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="btn-group">
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small className="text-body-secondary">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Wrapper>
+
+const Main = () => {
+    const dispatch = useDispatch();
+
+    const products = useSelector((state: RootState) => state.product.products);
+    const [searchText, setSearchText] = useState('');
+    const [selected, setSelected] = useState<number[]>([]);
+    const [notify, setNotify] = useState({
+        show: false,
+        error: false,
+        message: ''
+    });
+
+    // Fetch products based on searchText
+    useEffect(() => {
+        dispatch(fetchProducts(searchText));
+    }, [searchText, dispatch]);
+
+    const isSelected = (id: number) => selected.includes(id);
+
+    const select = (id: number) => {
+        if (isSelected(id)) {
+            setSelected(selected.filter(s => s !== id));
+        } else {
+            setSelected([...selected, id]);
+        }
+    };
+
+    const generate = async () => {
+        try {
+            const response = await axios.post('links', {
+                products: selected
+            });
+
+            setNotify({
+                show: true,
+                error: false,
+                message: `Link generated: ${process.env.CHECKOUT_URL}/${response.data.data.code}`
+            });
+        } catch (e) {
+            setNotify({
+                show: true,
+                error: true,
+                message: 'You should be logged in to generate a link!'
+            });
+        } finally {
+            setTimeout(() => {
+                setNotify({
+                    show: false,
+                    error: false,
+                    message: ''
+                });
+            }, 10000);
+        }
+    };
+
+    let button, info;
+
+    if (selected.length > 0) {
+        button = (
+            <div className="input-group-append">
+                <button className="btn btn-info" onClick={generate}>Generate Link</button>
+            </div>
         );
     }
+
+    if (notify.show) {
+        info = (
+            <div className="col-md-12 mb-4">
+                <div className={notify.error ? "alert alert-danger" : "alert alert-info"} role="alert">
+                    {notify.message}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <Wrapper>
+            <Header/>
+
+            <div className="album py-5 bg-light">
+                <div className="container">
+                    <div className="row">
+                        {info}
+
+                        <div className="col-md-12 mb-4 input-group">
+                            <input type="text" className="form-control" placeholder="Search"
+                                   onKeyUp={e => setSearchText((e.target as HTMLInputElement).value)}
+                            />
+                            {button}
+                        </div>
+
+                        {products.map((product: any) => (
+                            <div className="col-md-4" key={product.id}>
+                                <div
+                                    className={isSelected(product.id) ? "card mb-4 shadow-sm selected" : "card mb-4 shadow-sm"}
+                                    onClick={() => select(product.id)}
+                                >
+                                    <img src={product.image} height="200" alt={product.title} />
+                                    <div className="card-body">
+                                        <p className="card-text">{product.title}</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <small className="text-muted">${product.price}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    );
 }
 
 export default Main;
